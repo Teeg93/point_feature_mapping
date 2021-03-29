7c0975d421bbcc322d9f4dac1f2841669eba1638
Teeg93 <samuel_teague@live.com.au>
2021-03-29 10:51:12 +1030

    Slow but working

--- Point.py
+++ Point.py
@@ -25,6 +25,12 @@ class Point2D:
         dist = math.sqrt(dx*dx + dy*dy)
         return dist
 
+    def transformPoint(self,tx,ty,theta):
+        p = np.array([[self.x],[self.y],[1]]) #homogeneous representation of point
+        A = [[np.cos(theta), -np.sin(theta),tx],[np.sin(theta),np.cos(theta),ty],[0,0,1]] #homogeneous transform
+        p_prime = np.matmul(A,p)
+        return (Point2D(p_prime[0][0],p_prime[1][0]))
+
 class Cluster:
     def __init__(self,points=None):
         if points is None:
@@ -61,6 +67,84 @@ class Cluster:
                 else:
                     points[i], points[k - 1] = points[k - 1], points[i]  # swap k-1 with i
 
+    def transformCluster(self,tx,ty,theta):
+        points=[]
+        for p in self.points:
+            points.append(p.transformPoint(tx,ty,theta))
+        return Cluster(points)
+
+class LocalCluster:
+    def __init__(self,origin,threshold=2.0,neighbours=None):
+        if not isinstance(origin,Point2D):
+            print("WARNING: Expected LocalCluster(origin) argument 'origin' to be of type 'Point2D'")
+        self.origin = origin
+        if neighbours is not None:
+            self.neighbours=neighbours
+        else:
+            self.neighbours = []
+        self.threshold=threshold
+    def __getitem__(self, item):
+        if item==0:
+            return self.origin
+        else:
+            return self.neighbours[item]
+
+    def getNumberOfNeigbours(self):
+        return len(self.neighbours)
+    def addNeighbour(self,point):
+        if not isinstance(point,Point2D):
+            print("WARNING: Attempted to add neighbor to LocalCluster which is not of type Point2D.")
+            return
+        dx = point[0]-self.origin[0]
+        dy = point[1]-self.origin[1]
+        self.neighbours.append(Point2D(dx,dy))
+
+    def rotateLocalCluster(self,theta):
+        neighbours = []
+        for p in self.neighbours:
+            neighbours.append(p.transformPoint(0,0,theta))
+        return LocalCluster(self.origin,neighbours=neighbours)
+
+    def compareDistance(self,cluster):
+        used_idx=[]
+        ranges=[]
+        for i in range(len(self.neighbours)):
+            shortest_dist=np.inf
+            shortest_idx = None
+            for j in range(len(cluster.neighbours)):
+                if j in used_idx: #cannot reuse the same point
+                    continue
+                else:
+                    dist=self.neighbours[i].distanceTo(cluster.neighbours[j])
+                    #dist=angle(self.origin,self.neighbours[i],cluster.neighbours[j])
+                    if dist < self.threshold and dist < shortest_dist:
+                        shortest_dist=dist
+                        shortest_idx=j
+            if not shortest_dist==np.inf:
+                used_idx.append(shortest_idx)
+                ranges.append(shortest_dist)
+
+        if len(ranges) == 0:
+            return (9999,0)
+        distance = 0
+        numberOfMatches=0
+        for i in range(len(ranges)):
+            if ranges[i]<self.threshold:
+                distance += ranges[i]*ranges[i]
+                numberOfMatches+=1
+        distance=np.sqrt(distance)/numberOfMatches
+        return(distance,numberOfMatches)
+
+
+def dotproduct(v1, v2):
+  return sum((a*b) for a, b in zip(v1, v2))
+def length(v):
+  return math.sqrt(dotproduct(v, v))
+def angle(origin,p1,p2):
+    v1 = [p1[0] - origin[0], p1[1] - origin[1]]
+    v2 = [p2[0] - origin[0], p2[1] - origin[1]]
+    return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
+
 
 class Pair:
     def __init__(self,point_1,point_2):
@@ -74,3 +158,7 @@ class Pair:
     def distance(self):
         distance = self.point_1.distanceTo(self.point_2)
         return distance
+    def transformPair(self,tx,ty,theta):
+        p1 = self.point_1.transformPoint(tx,ty,theta)
+        p2 = self.point_2.transformPoint(tx,ty,theta)
+        return Pair(p1,p2)
--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
-from Point import Point2D, Cluster, Pair
+from .Point import Point2D, Cluster, Pair, LocalCluster
 import numpy as np
+import matplotlib.pyplot as plt
 def four_nearest_neighbours(M,m):
     if not (isinstance(M,list)):
         print(f"WARNING: Expected argument M to be of type 'list', received {type(M)}")
@@ -13,8 +14,19 @@ def four_nearest_neighbours(M,m):
     for i,point in enumerate(M):
         distances.append([m.distanceTo(M[i]),M[i]])
     distances=sorted(distances,key=lambda x: x[0]) #sort distances numerically
-    for i in range(0,4): #ignore the point matching itself
+    for i in range(0,4):
         C.addPoint(distances[i][1]) #add three nearest neighbours to point
+    return C
+
+def generateLocalCluster(M,m,k=15):
+    """ Generate a cluster with k points at coordinate origin point m"""
+    distances=[]
+    C = LocalCluster(m) #initialize origin
+    for i,point in enumerate(M):
+        distances.append([m.distanceTo(M[i]),M[i]])
+    distances=sorted(distances,key=lambda x:x[0])
+    for i in range(1,k+1):
+        C.addNeighbour(distances[i][1])
     return C
 
 def pairs_from_vector(V1,V2):
@@ -24,12 +36,12 @@ def pairs_from_vector(V1,V2):
     return pairs
 
 def evaluate_match(Kf):
-    sigma = 0.1
+    sigma = 2.0
     omit = 0
     E=0
     for i in range(len(Kf)):
         dist = Kf[i].distance()
-        if dist>1.0:
+        if dist>3.0:
             omit+=1
             continue
         else:
@@ -60,14 +72,71 @@ def generate_key_feature_list(M,D):
     ret = sorted(K,key=lambda x: x[1])
     return ret
 
+def visualizeLocalCluster(C):
+    x= [C[0][0]]
+    y= [C[0][1]]
+    for j in range(len(C.neighbours)):
+        x.append(x[0]+C.neighbours[j][0])
+        y.append(y[0]+C.neighbours[j][1])
+    plt.scatter(x,y)
+    plt.scatter(x[0],y[0])
+    plt.show()
+
+def visualizeLocalClusters(C1,C2):
+    x1= [C1[0][0]]
+    y1= [C1[0][1]]
+    x2= [C1[0][0]]
+    y2= [C1[0][1]]
+    for j in range(len(C1.neighbours)):
+        x1.append(x1[0]+C1.neighbours[j][0])
+        y1.append(y1[0]+C1.neighbours[j][1])
+    for j in range(len(C2.neighbours)):
+        x2.append(x2[0]+C2.neighbours[j][0])
+        y2.append(y2[0]+C2.neighbours[j][1])
+    plt.scatter(x1,y1)
+    plt.scatter(x2,y2)
+    plt.scatter(x1[0],y1[0])
+    plt.scatter(x2[0],y2[0])
+    plt.show()
+def samSearch(M,D):
+    Cm=[] #list of local clusters from model
+    Cd=[] #list of local clusterd from data
+    for i in range(len(D)):
+        Cd.append(generateLocalCluster(D,D[i]))
+    for j in range(len(M)):
+        Cm.append(generateLocalCluster(M,M[j]))
+    for i in range(len(Cd)): #for each data cluster
+        bestTheta = 0
+        bestDistance = np.inf
+        bestNumberOfMatches = 0
+        index = 0
+        bestRotatedClust = None
+        for j in range(len(Cm)): #search each model cluster
+            for theta in np.arange(0,2*np.pi,0.02): #rotate each cluster
+                rotatedCd = Cd[i].rotateLocalCluster(theta)
+                distance,numberOfMatches= rotatedCd.compareDistance(Cm[j])
+                if numberOfMatches >= bestNumberOfMatches:
+                    if distance < bestDistance:
+                        bestNumberOfMatches=numberOfMatches
+                        bestDistance=distance
+                        bestTheta=theta
+                        index = j
+                        bestRotatedClust = rotatedCd
+        print(f"Matched point {i} ({Cd[i][0][0]:.2f},{Cd[i][0][1]:.2f}) to {index} ({Cm[index][0][0]:.2f},{Cm[index][0][1]:.2f})")
+        print(f"Best theta: {2*np.pi-bestTheta}, Best Distance: {bestDistance}, Best Number of Matches: {bestNumberOfMatches}")
+        visualizeLocalClusters(bestRotatedClust,Cm[index])
+
 def self_test():
     M = []
     D = []
     for i in range(10):
         for j in range(1,10,3):
             M.append(Point2D(i,j))
-            D.append(Point2D(j,i))
-    generate_key_feature_list(M,D)
+            D.append(Point2D(i,j).transformPoint(0,2,4.9))
+    for point in D:
+        print(point[0],point[1])
+    samSearch(M,D)
+    #generate_key_feature_list(M,D)
 
 
 if __name__ == "__main__":
